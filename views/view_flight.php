427384d0c97557b088e8923d114e89cb5b50fec6
Léo <leo.zmoos@heig-vd.ch>
2023-01-24 19:40:47 +0100

    filters frontend almost done

--- views/view_flight.php
+++ views/view_flight.php
@@ -51,6 +51,147 @@ ob_start();
 .show {
 	display: block
 }
+
+/* Functional styling;
+ * These styles are required for noUiSlider to function.
+ * You don't need to change these rules to apply your design.
+ */
+.noUi-target,.noUi-target * {
+  -webkit-touch-callout: none;
+  -webkit-user-select: none;
+  -ms-touch-action: none;
+  touch-action: none;
+  -ms-user-select: none;
+  -moz-user-select: none;
+  user-select: none;
+  -moz-box-sizing: border-box;
+  box-sizing: border-box;
+}
+
+.noUi-target {
+  position: relative;
+  direction: ltr;
+}
+
+.noUi-base {
+  width: 100%;
+  height: 100%;
+  position: relative;
+  z-index: 1;
+/* Fix 401 */
+}
+
+.noUi-origin {
+  position: absolute;
+  right: 0;
+  top: 0;
+  left: 0;
+  bottom: 0;
+}
+
+.noUi-handle {
+  position: relative;
+  z-index: 1;
+}
+
+.noUi-stacking .noUi-handle {
+/* This class is applied to the lower origin when
+   its values is > 50%. */
+  z-index: 10;
+}
+
+.noUi-state-tap .noUi-origin {
+  -webkit-transition: left 0.3s,top .3s;
+  transition: left 0.3s,top .3s;
+}
+
+.noUi-state-drag * {
+  cursor: inherit !important;
+}
+
+/* Painting and performance;
+ * Browsers can paint handles in their own layer.
+ */
+.noUi-base,.noUi-handle {
+  -webkit-transform: translate3d(0,0,0);
+  transform: translate3d(0,0,0);
+}
+
+/* Slider size and handle placement;
+ */
+.noUi-horizontal {
+  height: 4px;
+}
+
+.noUi-horizontal .noUi-handle {
+  width: 18px;
+  height: 18px;
+  border-radius: 50%;
+  left: -7px;
+  top: -7px;
+  background-color: #345DBB;
+}
+
+/* Styling;
+ */
+.noUi-background {
+  background: #D6D7D9;
+}
+
+.noUi-connect {
+  background: #345DBB;
+  -webkit-transition: background 450ms;
+  transition: background 450ms;
+}
+
+.noUi-origin {
+  border-radius: 2px;
+}
+
+.noUi-target {
+  border-radius: 2px;
+}
+
+.noUi-target.noUi-connect {
+}
+
+/* Handles and cursors;
+ */
+.noUi-draggable {
+  cursor: w-resize;
+}
+
+.noUi-vertical .noUi-draggable {
+  cursor: n-resize;
+}
+
+.noUi-handle {
+  cursor: default;
+  -webkit-box-sizing: content-box !important;
+  -moz-box-sizing: content-box !important;
+  box-sizing: content-box !important;
+}
+
+.noUi-handle:active {
+  border: 8px solid #345DBB;
+  border: 8px solid rgba(53,93,187,0.38);
+  -webkit-background-clip: padding-box;
+  background-clip: padding-box;
+  left: -14px;
+  top: -14px;
+}
+
+/* Disabled state;
+ */
+[disabled].noUi-connect,[disabled] .noUi-connect {
+  background: #B8B8B8;
+}
+
+[disabled].noUi-origin,[disabled] .noUi-handle {
+  cursor: not-allowed;
+}
+
+
 </style>
 
 <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -175,7 +316,106 @@ ob_start();
 				</form>
 			</div>
 		</div>
-	</div>
+		<br>
+		<div class="container">
+		<div class="row">
+			<div class="booking-form">
+			<div class="row">
+					<h1> Voici la liste des vols disponible selon les critères: </h1><br>
+					<h3><b> Filtrer: </b></h3>
+					<h4> Par prix: </h4><br>
+					<div class="row-md-12">
+					<div class="col-md-4">
+						<h4> Fourchette: </h4>
+						<div class="container">
+						  <div class="row">
+							<div class="col-sm-12">
+							  <div id="slider-range"></div>
+							</div>
+						  </div>
+						  <div class="row slider-labels">
+							<div class="col-xs-6 caption">
+							  <strong>Min:</strong> <span id="slider-range-value1"></span>
+							</div>
+							<div class="col-xs-6 text-right caption">
+							  <strong>Max:</strong> <span id="slider-range-value2"></span>
+							</div>
+						  </div>
+						  <div class="row">
+							<div class="col-sm-12">
+							  <form>
+								<input type="hidden" name="min-value" value="">
+								<input type="hidden" name="max-value" value="">
+							  </form>
+							</div>
+						  </div>
+						</div>
+					</div>
+					<div class="col-md-2">
+						<label class="container">Croissant
+						  <input type="radio" checked="checked" name="radio">
+						  <span class="checkmark"></span>
+						</label>
+						<label class="container">Décroissant
+						  <input type="radio" name="radio">
+						  <span class="checkmark"></span>
+						</label>
+					</div>
+					
+					<h4> Par compagnie: </h4>
+					<div class="col-md-2">
+						<select name="compagnie"  id="compagnie" class="dropdownStyle">
+							<option value="swissair"> Swiss Air</option>
+							<option value="airfrance"> Air france </option>
+							<option value="skyexpress"> Sky Express </option>
+							<option value="sunexpress"> Sun Express </option>
+						</select>
+					</div>			
+				</div>
+			</div>
+				
+		<div class="row">
+		<div class="row-md-12">
+			<table>
+			<tr>
+				<td>
+					<h4> Escales: </h4>
+					<table>
+					<tr>
+						<td>
+							<input type="checkbox" id="direct" name="direct" checked>
+							<label for="direct">Vol direct</label>
+						</td>
+					</tr>
+					
+					<tr>
+						<td>
+							<input type="checkbox" id="onestopover" name="onestopover">
+							<label for="onestopover">Une escale</label>
+						</td>
+					</tr>
+					
+					<tr>
+						<td>
+							<input type="checkbox" id="twostopover" name="twostopover">
+							<label for="twostopover">Deux escales</label>
+						</td>
+					</tr>
+				</td>
+			
+				<td>
+					<h4> Temps de voyage: </h4>
+					<input type="number" id="tempsvoyage" name="tempsvoyage" step="1" placeholder="Nombre d'heures max">
+				</td>
+			</tr>
+				
+		</div>				
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
 </div>
 
 <script>
@@ -214,6 +454,53 @@ function calc() {
     txt.value = arr.join( ', ' )
 }
 </script>
+
+<script>
+const rangeInput = document.querySelectorAll(".range-input input"),
+  priceInput = document.querySelectorAll(".price-input input"),
+  range = document.querySelector(".slider .progress");
+let priceGap = 1000;
+
+priceInput.forEach((input) => {
+  input.addEventListener("input", (e) => {
+    let minPrice = parseInt(priceInput[0].value),
+      maxPrice = parseInt(priceInput[1].value);
+
+    if (maxPrice - minPrice >= priceGap && maxPrice <= rangeInput[1].max) {
+      if (e.target.className === "input-min") {
+        rangeInput[0].value = minPrice;
+        range.style.left = (minPrice / rangeInput[0].max) * 100 + "%";
+      } else {
+        rangeInput[1].value = maxPrice;
+        range.style.right = 100 - (maxPrice / rangeInput[1].max) * 100 + "%";
+      }
+    }
+  });
+});
+
+rangeInput.forEach((input) => {
+  input.addEventListener("input", (e) => {
+    let minVal = parseInt(rangeInput[0].value),
+      maxVal = parseInt(rangeInput[1].value);
+
+    if (maxVal - minVal < priceGap) {
+      if (e.target.className === "range-min") {
+        rangeInput[0].value = maxVal - priceGap;
+      } else {
+        rangeInput[1].value = minVal + priceGap;
+      }
+    } else {
+      priceInput[0].value = minVal;
+      priceInput[1].value = maxVal;
+      range.style.left = (minVal / rangeInput[0].max) * 100 + "%";
+      range.style.right = 100 - (maxVal / rangeInput[1].max) * 100 + "%";
+    }
+  });
+});
+</script>
+
+<script src="jquery-3.6.3.min.js" ></script>
+<script src="priceSlider.js" ></script>
 
 <?php $contenu = ob_get_clean(); // Stocke la page dans la variable
 require "layout.php";
